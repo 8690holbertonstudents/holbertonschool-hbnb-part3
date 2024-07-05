022978050068f50da371f97bd38caafb834c4750
8690holbertonstudents <8690@holbertonstudents.com>
2024-07-05 17:19:31 +0200

    update env and config file (resolve env variables data)

--- app/config.py
+++ app/config.py
@@ -8,6 +8,8 @@ from models import *
 import psycopg2
 import os
 
+load_dotenv()
+
 db = SQLAlchemy()
 
 # Generate and force directory for SQLite db
@@ -23,17 +25,17 @@ class Config:
     or production environment (Postgresql)
     """
     if os.environ.get('FLASK_ENV') == 'production':
-        load_dotenv('.env.prod')
         usr = os.environ.get('USERNAME')
         pwd = os.environ.get('PASSWORD')
+        db_host = os.environ.get('DB_HOST')
+        db_port = os.environ.get('DB_PORT')
         db_name = os.environ.get('DB_NAME')
 
-        SQLALCHEMY_DATABASE_URI = f'postgresql://{usr}:{pwd}@127.0.0.1:5432/{db_name}'
+        SQLALCHEMY_DATABASE_URI = f'postgresql://{usr}:{pwd}@{db_host}:{db_port}/{db_name}'
         engine = create_engine(SQLALCHEMY_DATABASE_URI)
         SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     elif os.environ.get('FLASK_ENV') == 'development':
-        load_dotenv('.env.dev')
         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
             os.path.join(datadir, 'development.db')
         engine = create_engine(SQLALCHEMY_DATABASE_URI)
